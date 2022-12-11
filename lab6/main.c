872a618fbadd6ace7ac9d22ba3823248a4447eba
Andrzej <228744@edu.p.lodz.pl>
2022-12-11 14:27:39 +0100

    Initial commit for lab6

---
+++ lab6/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int my_printf(char *format_string, int param){
+	int j=0;
+	for(int i=0;i<strlen(format_string);i++){
+		if((format_string[i] == '#') && (format_string[i+1] == 'g')){
+			i++;
+			int temp;
+			int tab[1000];
+			for(j=0;;j++){
+				
+				temp=param % 10;
+				if(temp==0){
+					temp=9;
+				}
+				else{
+					temp-=1;
+				}
+				tab[j]=temp;
+				
+				if((param=param/10) == 0) break;
+			}
+			for(int k=j;k>=0;k--){
+				printf("%d", tab[k]);	
+			}
+		
+		}else
+			putchar(format_string[i]);
+	}
+	puts("");
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	char buf[1024];
+	int buf2=0;
+	while(gets(buf)){
+		if((scanf("%d", &buf2)) == 0) {printf("Podales nie liczbe\n");return 1;}
+		my_printf(buf,buf2);
+		getchar();
+	}
+	return 0;
+}
