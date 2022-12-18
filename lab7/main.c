d348e6b376029f44fd5fb42e06741488ea94b3b0
Andrzej <228744@edu.p.lodz.pl>
2022-12-18 15:02:25 +0100

    Creating method for changing hexadecimal characters

--- lab7/main.c
+++ lab7/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int my_printf(char *format_string, char param){
+int my_printf(char *format_string, char *param){
 	int j=0;
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'j'){
@@ -11,14 +11,36 @@ int my_printf(char *format_string, char param){
 			char tab[1000];
 			for(j=0;;j++){
 				
-				char[tab]=param % 10;
-				if(temp==0){
-					result=9;
+				if((param[j]<48 || param[j]>57) && (param[j]<65 || param[j]>70))
+				{
+					printf("Not hexadecimal character");
+					return 1;
 				}
-				else{
-					result = (temp*9+1)%10;
+				
+				switch(param[j])
+				{
+					case 65:
+						tab[j] = 71;
+						break;
+					case 66:
+						tab[j] = 72;
+						break;
+					case 67:
+						tab[j] = 73;
+						break;
+					case 68:
+						tab[j] = 74;
+						break;
+					case 69:
+						tab[j] = 75;
+						break;
+					case 70:
+						tab[j] = 76;
+						break;
+					default:
+						printf("Not hexadecimal character");
+						return 1;
 				}
-				tab[j]=result;
 				
 				if((param=param/10) == 0) break;
 			}
